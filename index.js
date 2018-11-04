bcc474a0c99c6074279e40860f5c4f989e0331f8
PetePhx <pete0phx@yandex.com>
2018-11-03 17:27:21 -0700

    database integration of genres

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const genres = require('./routes/genres');
+// const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+  .then(() => console.log('Connected to the vidly DB.'))
+  .catch(err => console.error('Error: Could Not Connect.', err.message));
 
 app.use(express.json());
 app.use('/api/genres', genres);
+// app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
