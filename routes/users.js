f6fb83e23baef9e439d306ae02325ddbc6397563
PetePhx <pete0phx@yandex.com>
2018-11-09 10:55:16 -0700

    secure user id handling via token at /api/users/me

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-// const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User, validate } = require('../models/user');
+const auth = require('../middleware/auth');
+
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 
 router.get('/', async (req, res) => {
   const users = await User.find().sort({ name: 1 });
