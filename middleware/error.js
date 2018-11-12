aa4d013eb73b5dd65fe15254c2fb17fac427fb42
PetePhx <pete0phx@yandex.com>
2018-11-12 11:48:52 -0700

    logging errors to mongodb using winston-mongodb

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,14 @@
 const { createLogger, format, transports } = require('winston');
+require('winston-mongodb');
 
 const logger = createLogger({
   level: 'info',
   format: format.json(),
   transports: [
     new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' })
+    new transports.File({ filename: 'combined.log' }),
+    new transports.MongoDB({ db: 'mongodb://localhost/vidly', level: 'error',
+      options: { useNewUrlParser: true } })
   ]
 });
 
@@ -14,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 module.exports = function (err, req, res, next) {
-  logger.log('error', err.message);
-
+  logger.log('error', err.message, err);
+  
   res.status(500).send('something failed.');
 }
