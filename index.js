803dbb5441d828b87ad71fa1c8283bc497f966f9
PetePhx <pete0phx@yandex.com>
2018-11-13 11:11:41 -0700

    refactor: validation logic

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const { createLogger, format, transports } = require('winston');
 
 require('./startup/logging');
 require('./startup/routes.js')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/validation')();
 
 process.on('unhandledRejection', (exc) => { throw(exc) });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => {
+  createLogger({ transports: new transports.Console(), format: format.cli() })
+    .info(`Listening on port ${port}...`)
+});
--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
-const { createLogger, transports } = require('winston');
+const { createLogger, format, transports } = require('winston');
 const dbLogger = createLogger({ transports: new transports.File({ filename: 'combined.log' }) });
+
+if (process.env.NODE_ENV !== 'production') {
+  dbLogger.add(new transports.Console({ format: format.cli() }));
+}
 
 module.exports = function () {
   mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useCreateIndex: true, })
