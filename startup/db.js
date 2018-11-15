6d74cef861fdc82b4b0c4823bf00fd796e5dd9df
PetePhx <pete0phx@yandex.com>
2018-11-15 14:50:01 -0700

    setting up the integration test environment

--- startup/db.js
+++ startup/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const { createLogger, format, transports } = require('winston');
 const dbLogger = createLogger({ transports: new transports.File({ filename: 'combined.log' }) });
+const config = require('config');
 
 if (process.env.NODE_ENV !== 'production') {
   dbLogger.add(new transports.Console({ format: format.cli() }));
 }
 
 module.exports = function () {
-  mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useCreateIndex: true, })
-  .then(() => dbLogger.info('Connected to the vidly DB.'));
+  const db = config.get('db');
+  mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, })
+  .then(() => dbLogger.info(`Connected to ${db}...`));
 }
