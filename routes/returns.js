da050c5c0fbd766d4c9fddb4fe2ce96399c27fd4
PetePhx <pete0phx@yandex.com>
2018-11-18 16:56:34 -0700

    returns endpoint + test refactor

--- routes/returns.js
+++ routes/returns.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
-router.post('/', async (req, res) => {
-  res.status(401).send('Unauthorized');
+router.post('/', auth, async (req, res) => {
+  if(!req.body.customerId)
+    res.status(400).send('Bad Request: customerId missing');
+
+  if(!req.body.movieId)
+    res.status(400).send('Bad Request: movieId missing');
+
+  // res.status(401).send('Unauthorized');
 });
 
 module.exports = router;
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const { Rental } = require('../../models/rental');
+const { User } = require('../../models/user');
 const mongoose = require('mongoose');
 
 describe('/api/returns', () => {
@@ -7,11 +8,13 @@ describe('/api/returns', () => {
   let customerId;
   let movieId;
   let rental;
+  let token;
 
   beforeEach(async () => {
     server = require('../../index');
     customerId = new mongoose.Types.ObjectId();
     movieId = new mongoose.Types.ObjectId();
+    token = (new User()).generateAuthToken();
 
     rental = new Rental({
       customer: {
@@ -27,6 +30,13 @@ describe('/api/returns', () => {
     });
     await rental.save();
   });
+
+  const exec = () => {
+    return request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({ customerId, movieId });
+  };
   
   afterEach(async () => {
     await Rental.deleteMany({});
@@ -34,10 +44,24 @@ describe('/api/returns', () => {
   });
 
   it('should return 401 if client not logged in', async () => {
-    const response = await request(server)
-      .post('/api/returns')
-      .send({ customerId, movieId });
+    token = '';
+    
+    const response = await exec();
     
     expect(response.status).toBe(401);
+  });
+
+  it('should return 400 if customerId not provided', async () => {
+    customerId = '';
+    const response = await exec();
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return 400 if movieId not provided', async () => {
+    movieId = '';
+    const response = await exec();
+
+    expect(response.status).toBe(400);
   });
 });
