9614d7e6a2e39c14a0f483b1a13b3c9eab99d79f
PetePhx <pete0phx@yandex.com>
2018-11-18 17:48:16 -0700

    rental return end point + integration test

--- routes/returns.js
+++ routes/returns.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const { Rental } = require('../models/rental');
 
 router.post('/', auth, async (req, res) => {
-  if(!req.body.customerId)
-    res.status(400).send('Bad Request: customerId missing');
+  if(!req.body.customerId) return res.status(400).send('Bad Request: customerId missing');
+  if(!req.body.movieId) return res.status(400).send('Bad Request: movieId missing');
 
-  if(!req.body.movieId)
-    res.status(400).send('Bad Request: movieId missing');
+  const rental = await Rental.findOne({ 
+    'customer._id': req.body.customerId,
+    'movie._id': req.body.movieId,
+  });
 
-  // res.status(401).send('Unauthorized');
+  if (!rental) return res.status(404).send('No rental with the given properties found.');
+  if (rental.dateReturned) return res.status(400).send('rental return is already processed.');
+
+  rental.dateReturned = Date.now();
+  await rental.save();
+  res.status(200).send('rental return successful.');
 });
 
 module.exports = router;
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -45,7 +45,7 @@ describe('/api/returns', () => {
 
   it('should return 401 if client not logged in', async () => {
     token = '';
-    
+
     const response = await exec();
     
     expect(response.status).toBe(401);
@@ -63,5 +63,31 @@ describe('/api/returns', () => {
     const response = await exec();
 
     expect(response.status).toBe(400);
+  });
+
+  it('should return 404 if it can\'t find a rental with the given props', async () => {
+    customerId = new mongoose.Types.ObjectId();
+    movieId = new mongoose.Types.ObjectId();
+
+    const response = await exec();
+
+    expect(response.status).toBe(404);
+  });
+
+  it('should return 400 if return is already processed', async () => {
+    await Rental.findOneAndUpdate({
+      'customer._id': customerId,
+      'movie._id': movieId,
+    }, { dateReturned: Date.now() });
+
+    const response = await exec();
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return 200 if a valid return', async () => {
+    const response = await exec();
+
+    expect(response.status).toBe(200);
   });
 });
