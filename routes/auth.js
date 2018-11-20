906a89e13a93ddd7eadc05cd14cc6711bcd6a9f8
PetePhx <pete0phx@yandex.com>
2018-11-20 09:39:27 -0700

    replace bcrypt with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Joi = require('joi');
 const { User } = require('../models/user');
 
